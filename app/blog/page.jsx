7e7a438298d57926c1f226640f757de167f83979
Zobaidul Kazi <experiment.zobaidulkazi@gmail.com>
2025-01-24 04:38:36 +0000

    blog build

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,5 @@
 // app/blog/page.js
-
+"use client"
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
---
+++ env.ex
@@ -0,0 +1,3 @@
+MONGODB_URI=mongodb+srv://exzoba_______________
+
+NEXT_PUBLIC_API_URL=https://zobjs.github.io/nextjs-getStaticParams-lesson
